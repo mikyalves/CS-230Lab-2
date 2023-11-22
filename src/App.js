eaaf15ca3ab372e704848e6d58d190f740ac4f54
mikyalves <msa0043@mix.wvu.edu>
2023-11-22 18:42:07 -0500

    Lab 08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
+
 
 function App() {
+  const [task, setTask] = useState([]);
+  const todoInputRef = useRef();
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = todoInputRef.current.value;
+    if (newItem !== "") {
+      setTask(prevTask => [...prevTask, newItem]);
+    }
+    todoInputRef.current.value = "";
+  };
+
+  const removeItem = (index, e) => {
+    e.stopPropagation();
+    const updatedTasks = task.filter((_, indx) => indx !== index);
+    setTask(updatedTasks);
+  };
+
+  const renderTasks = () => {
+    return task.map((task, index) => (
+      <li key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
+        {task}
+        <button className="remove" onClick={(e) => removeItem(index, e)}>x</button>
+      </li>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="ToDo">
+        <h1>ToDo List</h1>
+        <input type="text" className="enterItem" placeholder="Enter an item" ref={todoInputRef} />
+        
+        <ul className="todo-list">{renderTasks()}</ul>
+        <button className="btn" onClick={addItem}>
+          Add
+        </button>
+
+      </div>
   );
-}
+};
 
 export default App;
